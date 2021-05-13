5841d966e1e4894632209bbc66bbdda4e1a1bc62
Scott Lee <scott.lee@chghealthcare.com>
2021-05-13 11:19:40 -0600

    feat: add mushrooms

--- main/makedudewalk.js
+++ main/makedudewalk.js
@@ -10,6 +10,7 @@ const FACING_LEFT = 3;
 const FACING_RIGHT = 1;
 const FRAME_LIMIT = 12;
 const MOVEMENT_SPEED = 2;
+const bulletTotal = 0;
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
@@ -21,9 +22,21 @@ let positionX = 0;
 let positionY = 0;
 let img = new Image();
 
+let imgMushroom = new Image();
+
 let player1 = new Player(2.5, 2, 0,0);
 let player2 = new Player(2.5, 2, 550,550);
+width = 1300,
+height = 700,
 
+//arrays to store game element objects
+bullets = [];
+mushrooms = [];
+
+function Mushroom(xLocation, yLocation){
+  this.xLocation = xLocation;
+  this.yLocation = yLocation;
+}
 
 function Player(scale,movementSpeed,startingPositionX,startingPositionY) {
   this.scale = scale;
@@ -47,9 +60,9 @@ function keyUpListener(event) {
     keyPresses[event.key] = false;
 }
 
-function loadImage() {
-  console.log('im here');
+function loadImages() {
   img.src = 'MainGuySpriteSheet.png';
+  imgMushroom.src = 'mush.png'
   img.onload = function() {
     window.requestAnimationFrame(gameLoop);
   };
@@ -61,7 +74,7 @@ function drawFrame(frameX, frameY, canvasX, canvasY) {
                 canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
 }
 
-loadImage();
+loadImages();
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -140,8 +153,56 @@ function gameLoop() {
     player2.currentLoopIndex = 0;
   }
 
+  // code for bullet
+  if (keyPresses[' ']&& bullets.length < 1) {
+    //if (keyPresses[' ']) {
+    let b = {
+     //x: player_x + player_w/2, y: player_y + player_h/2, speedX: 0, speedY: 0, width: 20, height: 20
+     x: player1.positionX + WIDTH/2 + 17, y: player1.positionY + HEIGHT/2, speedX: 0, speedY: 0, width: 20, height: 20
+    };
+    //b.x += WIDTH;
+     //b.speedX += 20;
+     //b.height = 5;
+    
+    if(player1.currentDirection == FACING_RIGHT) {
+      b.x += WIDTH/2 - 10;
+      b.speedX += 20;
+      b.height = 5;
+    }  
+    else if(player1.currentDirection == FACING_UP) {
+     b.y -= 10;
+     b.speedY -= 20;
+     b.width = 5;
+    }
+    else if(player1.currentDirection == FACING_DOWN && b.speedY === 0) {
+     //b.y += 10;
+     b.speedY += 20;
+     b.width = 5;
+    }
+    else if(b.speedX === 0 && (player1.currentDirection == FACING_LEFT  || b.speedY === 0)) {
+     b.x -= WIDTH/2;
+     b.speedX -= 20;
+     b.height = 5;
+    }
+    
+    bullets.push(b);
+  }
+ 
+
+
+
+  movebullet()
   drawFrame(CYCLE_LOOP[player1.currentLoopIndex], player1.currentDirection, player1.positionX, player1.positionY);
   drawFrame(CYCLE_LOOP[player2.currentLoopIndex], player2.currentDirection, player2.positionX, player2.positionY);
+
+  //draw the mushrooms
+  for (var i = 0; i < mushrooms.length; i++) {
+    ctx.drawImage(imgMushroom, mushrooms[i].xLocation, mushrooms[i].yLocation,22,22);
+
+  }
+  
+
+  drawbullet()
   window.requestAnimationFrame(gameLoop);
 }
 
@@ -157,4 +218,52 @@ function moveCharacter(deltaX, deltaY, direction, thePlayer) {
     thePlayer.positionY += deltaY;
   }
   thePlayer.currentDirection = direction;
+}
+
+function drawbullet() {
+  
+  for (var i = 0; i < bullets.length; i++) {
+   ctx.fillStyle = '#f00';
+   ctx.fillRect(bullets[i].x,bullets[i].y,bullets[i].width,bullets[i].height)
+  }
+}
+
+function movebullet() {
+  const pixelsFromEdge = 22;
+  if(bullets){
+    for (var i = 0; i < bullets.length; i++) {
+      bullets[i].x += bullets[i].speedX;
+      bullets[i].y += bullets[i].speedY;
+      let tempx
+      let tempy
+      if(bullets[i].x < 0 || bullets[i].x > width ||
+        bullets[i].y < 0 || bullets[i].y > height){
+        //bullet has got to end of screen 
+        //drop a mushroom location
+        if(bullets[i].x < 0){
+          tempx = 0
+          tempy = bullets[i].y 
+        }else 
+        if(bullets[i].x > width) {
+          tempx = width - pixelsFromEdge
+          tempy = bullets[i].y 
+        }else
+        if(bullets[i].y < 0 ) {
+          tempx = bullets[i].x 
+          tempy = 0
+        }else
+        if(bullets[i].y > height) {
+          tempx = bullets[i].x 
+          tempy = height-pixelsFromEdge
+        }
+
+        mushrooms.push(new Mushroom(tempx,tempy))
+        console.log(mushrooms)
+
+        bullets.splice(i, 1);
+        
+        
+      }
+    }
+  }
 }
